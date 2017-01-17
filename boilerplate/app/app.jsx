4557c2cff6b8e7cbe32e158ef245b892048a52bd
Jimmy <jimmyvoskuil@msn.com>
2017-01-17 09:59:23 +0100

    added test git

--- boilerplate/app/app.jsx
+++ boilerplate/app/app.jsx
@@ -2,8 +2,9 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
 
+// test
 
 ReactDOM.render(
-  <h1> Boiletplate app!</h1>,
+  <h1> Boiletplate React app!</h1>,
   document.getElementById('app')
 )
--- boilerplate/server.js
+++ boilerplate/server.js
@@ -1,13 +1,15 @@
 const express = require('express')
 
 // create app
+//testing github 
 let app = express()
 
 app.use(express.static("public"))
 
 
-
-
 app.listen(3000, function () {
 	console.log("App is running on 3000")
 })
+
+
+// ABC
